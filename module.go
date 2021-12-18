e92a772c7ab2dfc3573b5aef802ac204992ed0c1
比特虫洞 <43868880+bitwormhole@users.noreply.github.com>
2021-12-18 16:32:16 +0800

    update to v0.0.6

--- module.go
+++ module.go
@@ -9,8 +9,8 @@ import (
 
 const (
 	myName     = "github.com/bitwormhole/starter-security"
-	myVersion  = "v0.0.5"
-	myRevision = 5
+	myVersion  = "v0.0.6"
+	myRevision = 6
 )
 
 //go:embed src/main/resources
