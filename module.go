c354247cfa83569bd1153792656a9c5f6b096cbb
比特虫洞 <43868880+bitwormhole@users.noreply.github.com>
2022-02-11 21:39:05 +0800

    update to v0.1.2

--- module.go
+++ module.go
@@ -11,8 +11,8 @@ import (
 
 const (
 	myName     = "github.com/bitwormhole/starter-security"
-	myVersion  = "v0.1.1"
-	myRevision = 8
+	myVersion  = "v0.1.2"
+	myRevision = 9
 )
 
 ////////////////////////////////////////////////////////////////////////////////
